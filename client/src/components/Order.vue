7b954cbda91704f5fabd0c3612bf72ae023c266d
xuyang <xyy.hit@126.com>
2018-07-29 19:35:40 +0800

    1.fix

--- client/src/components/Order.vue
+++ client/src/components/Order.vue
@@ -9,8 +9,8 @@
 
       <el-form-item label="交易方式">
         <el-radio-group v-model="form.user">
-          <el-radio label="0">账户A先买 {{ myBalances[0] }}</el-radio>
-          <el-radio label="1">账户B先买 {{ myBalances[1] }}</el-radio>
+          <el-radio disabled label="0">账户A {{ myBalances[0] }}</el-radio>
+          <el-radio disabled label="1">账户B {{ myBalances[1] }}</el-radio>
         </el-radio-group>
       </el-form-item>
 
@@ -23,7 +23,8 @@
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">开始操作</el-button>
+        <el-button type="primary" @click="onSubmitA">账户A先买</el-button>
+        <el-button type="primary" @click="onSubmitB">账户B先买</el-button>
         <el-button @click="onQuery">查询余额</el-button>
       </el-form-item>
     </el-form>
@@ -34,26 +35,27 @@
 export default {
   data() {
     return {
-      form: { market: "336", user: "0", price: "", amount: "" },
+      form: { market: "336", user: "", price: "", amount: "" },
       myBalances: []
     };
   },
+  created() {
+    this.onQuery();
+  },
   methods: {
-    onSubmit() {
-      console.log("form data ===>" + this.form);
+    onSubmitA() {
       let config = {
         method: "post",
         url: "/zbg/placeOrder",
         data: {
           amount: this.form.amount, //下单数量
-          user: this.form.user, //买卖类型：0 卖出 1 购买
+          user: 0, //买卖类型：0 卖出 1 购买
           market: this.form.market, //市场ID
           price: this.form.price
         }
       };
       this.$ajax(config).then(response => {
         let resData = response.data;
-        console.log("resData ===>" + JSON.stringify(resData));
         if (resData.success) {
           this.$notify({
             title: "成功",
@@ -69,6 +71,42 @@ export default {
             duration: 0
           });
         }
+        this.form.amount = config.data.amount;
+        this.form.price = config.data.price;
+        this.onQuery();
+      });
+    },
+    onSubmitB() {
+      let config = {
+        method: "post",
+        url: "/zbg/placeOrder",
+        data: {
+          amount: this.form.amount, //下单数量
+          user: 1, //买卖类型：0 卖出 1 购买
+          market: this.form.market, //市场ID
+          price: this.form.price
+        }
+      };
+      this.$ajax(config).then(response => {
+        let resData = response.data;
+        if (resData.success) {
+          this.$notify({
+            title: "成功",
+            message: resData.msg,
+            type: "success",
+            duration: 0
+          });
+        } else {
+          this.$notify({
+            title: "失败",
+            message: resData.msg,
+            type: "error",
+            duration: 0
+          });
+        }
+        this.form.amount = config.data.amount;
+        this.form.price = config.data.price;
+        this.onQuery();
       });
     },
     onQuery() {
